5e2df6cc4eaeabf716ff0046156bf8ca2cd5224e
phihash <insharp0220@gmail.com>
2025-09-30 01:18:51 +0900

    css調整、コンポーネント一旦追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,42 @@
 import { useRetireContext } from "./contexts/RetireProvider";
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   const { status, setStatus } = useRetireContext();
 
   return (
     <>
-      <button
-        onClick={() => {
-          setStatus("退職");
-        }}
-      >
-        退職
-      </button>
-      <button
-        onClick={() => {
-          setStatus("転職活動中");
-        }}
-      >
-        転職活動中
-      </button>
-      <button
-        onClick={() => {
-          setStatus("休職");
-        }}
-      >
-        休職
-      </button>
+      <Header />
+      <Hero />
+      <div className="flex gap-2 w-full justify-center md:justify-start">
+        <button
+          className="bg-green-600 hover:bg-green-500  transition-colors duration-100 ease-out focus:bg-green-900 px-4 py-2 rounded-3xl font-bold text-white cursor-pointer"
+          onClick={() => {
+            setStatus("退職");
+          }}
+        >
+          退職
+        </button>
+        <button
+          className="bg-green-600 hover:bg-green-500  transition-colors duration-100 ease-out focus:bg-green-900 px-4 py-2 rounded-3xl font-bold text-white cursor-pointer"
+          onClick={() => {
+            setStatus("転職活動中");
+          }}
+        >
+          転職活動中
+        </button>
+        <button
+          className="bg-green-600 hover:bg-green-500  transition-colors duration-100 ease-out focus:bg-green-900 px-4 py-2 rounded-3xl font-bold text-white cursor-pointer"
+          onClick={() => {
+            setStatus("休職");
+          }}
+        >
+          休職
+        </button>
+      </div>
+
       <div className="text-xl">私は今 {status}です</div>
     </>
   );
