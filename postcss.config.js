bd9ab2a61f5cb72632b3115e8b08aa8cd3ae2b78
phihash <insharp0220@gmail.com>
2025-09-30 01:20:36 +0900

    設定追加

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // ← ここが v4 から必須
+    autoprefixer: {},
+  },
+};
---
+++ src/components/Hero.tsx
@@ -0,0 +1,3 @@
+export default function Hero() {
+  return <div className="bg-green-100">s</div>;
+}
