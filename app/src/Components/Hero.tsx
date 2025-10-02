7ccacd8d2acedae515eda6a8b42a220238beb845
phihash <insharp0220@gmail.com>
2025-10-02 13:41:56 +0900

    update .gitignore

--- app/src/Components/Hero.tsx
+++ app/src/Components/Hero.tsx
@@ -1,9 +1,17 @@
 "use client";
+import Image from "next/image";
+import heroImage from "@/public/hero.png";
+
 export default function Hero() {
   return (
     <div className="w-full ">
       <div className="max-w-screen-sm mx-auto mx-auto md:flex items-center justify-center gap-6 ">
-        <img className="block mx-auto py-4" src="/hero.png" alt="" />
+        <Image
+          className="block mx-auto py-4"
+          src={heroImage}
+          alt="Hero"
+          priority
+        />
       </div>
     </div>
   );
